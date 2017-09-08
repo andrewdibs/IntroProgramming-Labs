be0d43c28357bfaccbc970860850ba9f62d61588
Andrew DiBella <Andrew.DiBella1@marist.edu>
2017-09-08 12:02:21 -0400

    Solution to mad Libs exercise

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#Mad Libs
+#Author: Andrew DiBella
+#Date: 9 September 2017
+
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+
+    print("Make sure you " + verb + " your " +adjective +" " +noun + " at the " + place)
+main()
