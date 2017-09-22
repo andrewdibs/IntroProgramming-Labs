0b67e4547117b6a2ba83d039886dd1c8b707b29f
Andrew DiBella <Andrew.DiBella1@marist.edu>
2017-09-22 11:59:24 -0400

    Usability improvements to the guessinf game, resolves #4 and resolves #5.

--- guessing-game.py
+++ guessing-game.py
@@ -14,14 +14,22 @@ def main():
     while answer:
         guess = input("Can you guess what animal I'm thinking of?\n"
                       "Enter Animal: ").lower()
-        
-        if guess == animal:
-            answer = False
-            print("Wow, you got it.")
-            
-        elif guess == "quit":
+        if guess[0] == "q":
             answer = False
             print("Thanks for playing.")
+
+        elif guess == animal:
+            answer = False
+            print("Wow, you got it.")
+
+            response = input("Do you like this animal? 'y' or 'n': ")
+
+            if response == "y":
+                print("Good Answer..Me too.")
+
+            else:
+                print("Wrong answer.....")
+                
 
         else:
             print("Incorrect.. Try Again\n")
