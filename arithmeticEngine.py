590281b239eabaf267c1dacf658a1080729a25a0
Andrew DiBella <andrew.dibella1@marist.edu>
2017-10-13 11:40:57 -0400

    Created getInput getOperation and showIntro functions and began main loop

---
+++ arithmeticEngine.py
@@ -0,0 +1,69 @@
+#numberEngine
+#Author: Andrew DiBella
+#Date: Oct 13 2017
+
+#Intro ---Loop ---End
+#Make functions 
+#Ask for input
+#Ask for two numbers 
+#perform operation or quit 
+#show result 
+#
+
+
+def showIntro():
+    print("\t\t\tArithmetic Engine\n"
+          "\t\t\t================\n\n"
+          "Arithmetic engine allows user to perform basic operations on two numbers.")
+
+def getOperation():
+
+    userInput = input("Please Choose operation:\n\n"
+                      "Add\n"
+                      "Multiply\n"
+                      "Subtract\n"
+                      "Divide\n"
+                      "Enter quit to exit\n"
+                      ">>: ").lower()
+
+    return userInput
+                      
+                      
+
+
+def getInput():
+    nums = []
+    nums.append(int(input("Please enter first number: ")))
+    nums.append(int(input("Please enter second number: ")))
+
+    return nums
+
+
+
+showIntro()
+
+while True:
+    
+    operation = getOperation()
+    numbers = getInput()
+
+    if operation == "add":
+        print("Result = ", numbers[0] + numbers[1], "\n\n")
+
+    elif operation == "subtract":
+        print("Result = ",numbers[0] - numbers[1],"\n\n")
+
+
+
+
+        
+              
+
+
+
+
+
+
+
+
+
